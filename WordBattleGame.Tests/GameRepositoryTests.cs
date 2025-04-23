568224e64c2bcff16de675bd8cb0852744b70167
Iqbal Shafiq <iqbal.syafiq.rozaan@gmail.com>
2025-04-23 16:49:26 +0700

    chore: Remove unused using directives in GameRepositoryTests and add solution file for project structure

--- WordBattleGame.Tests/GameRepositoryTests.cs
+++ WordBattleGame.Tests/GameRepositoryTests.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using WordBattleGame.Data;
 using WordBattleGame.Models;
 using WordBattleGame.Repositories;
-using Xunit;
 
 namespace WordBattleGame.Tests
 {

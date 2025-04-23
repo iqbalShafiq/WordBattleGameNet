72694f5349cdb5c53754827a4c923e72dfb97ea1
Iqbal Shafiq <iqbal.syafiq.rozaan@gmail.com>
2025-04-23 15:12:45 +0700

    feat: Update JSON serialization settings to ignore cycles in controller responses

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddDatabase(builder.Configuration);
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
         {
-            options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+            options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
         });
 builder.Services.AddSignalR();
 builder.Services.AddEndpointsApiExplorer();
